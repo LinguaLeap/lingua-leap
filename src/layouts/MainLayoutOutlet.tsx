f6e026a6d71b94799b0f5d62cf0ca1970a063740
muhammeddilaver <muhammed.dilaver@gmail.com>
2024-01-06 14:40:10 -0500

    Fix template

--- src/layouts/MainLayoutOutlet.tsx
+++ src/layouts/MainLayoutOutlet.tsx
@@ -3,21 +3,21 @@ import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/common/Footer";
 
 function MainLayoutOutlet() {
-  const location = useLocation();
-  const isMessagesOrCommunityPage =
-    location.pathname === "/messages" || location.pathname === "/community";
+    const location = useLocation();
+    const isMessagesOrCommunityPage =
+        location.pathname === "/messages" || location.pathname === "/community";
 
-  return (
-    <div className="flex flex-col h-screen bg-light dark:bg-sky-blue-750">
-      <Header />
+    return (
+        <div className="flex flex-col min-h-screen bg-light dark:bg-sky-blue-750">
+            <Header />
 
-      <main className="flex flex-1 items-center justify-center">
-        <Outlet />
-      </main>
+            <main className="flex flex-1">
+                <Outlet />
+            </main>
 
-      {isMessagesOrCommunityPage ? null : <Footer />}
-    </div>
-  );
+            {isMessagesOrCommunityPage ? null : <Footer />}
+        </div>
+    );
 }
 
 export default MainLayoutOutlet;
